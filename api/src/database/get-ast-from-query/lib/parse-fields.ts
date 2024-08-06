MarcusViniciusBispoDosSantos/Directus

commit 3376349cbcb20a5a667e374d2c9b71fd666a4fd9

Fix server error on root m2a query (#23181)

api/src/database/get-ast-from-query/lib/parse-fields.ts
@@ -136,6 +136,12 @@ export async function parseFields(
 				}
 			}
 
+			if (name.includes(':')) {
+				const [key, scope] = name.split(':');
+				relationalStructure[key!] = { [scope!]: [] };
+				continue;
+			}
+
 			children.push({ type: 'field', name, fieldKey, whenCase: [] });
 		}
 	}

commit 545eb53a76ce6d860d846d07a62cc03aa5489ac6

Fix M2A query regression from #23181 (#23195)

api/src/database/get-ast-from-query/lib/parse-fields.ts
@@ -137,8 +137,14 @@ export async function parseFields(
 			}
 
 			if (name.includes(':')) {
-				const [key, scope] = name.split(':');
-				relationalStructure[key!] = { [scope!]: [] };
+				const [key, scope] = name.split(':') as [string, string];
+
+				if (key in relationalStructure === false) {
+					relationalStructure[key] = { [scope]: [] };
+				} else if (scope in (relationalStructure[key] as CollectionScope) === false) {
+					(relationalStructure[key] as CollectionScope)[scope] = [];
+				}
+
 				continue;
 			}
 